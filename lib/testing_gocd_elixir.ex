b47b5ea3a57cbbef33a3e84f7d5d83e9e0c052cc
Andrew Summers <oasummerso@gmail.com>
2019-05-04 11:04:22 -0500

    Initial commit

---
+++ lib/testing_gocd_elixir.ex
@@ -0,0 +1,18 @@
+defmodule TestingGocdElixir do
+  @moduledoc """
+  Documentation for TestingGocdElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestingGocdElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/testing_gocd_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule TestingGocdElixirTest do
+  use ExUnit.Case
+  doctest TestingGocdElixir
+
+  test "greets the world" do
+    assert TestingGocdElixir.hello() == :world
+  end
+end
